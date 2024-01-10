d051f69e0d98563afabbe5bc11303bbe81017803
Timo Reymann <timo.reymann@deepl.com>
2024-01-10 14:07:56 +0100

    feat: Add toml config file

--- UniLaunch.Core/Storage/StorageException.cs
+++ UniLaunch.Core/Storage/StorageException.cs
@@ -2,5 +2,9 @@ namespace UniLaunch.Core.Storage;
 
 public class StorageException : Exception
 {
-    
+    public StorageException(string message) 
+        : base(message) { }
+
+    public StorageException(string message, Exception e)
+        : base(message, e) { }
 }
