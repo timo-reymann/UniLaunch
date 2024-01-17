4345259ee5bf948061d6ef409f92e82af55104d6
Timo Reymann <timo.reymann@deepl.com>
2024-01-18 00:19:33 +0100

    fix: Fix autostart

--- UniLaunch.MacOS/Autostart/SharedListFileAutoStartRegistrationProvider.cs
+++ UniLaunch.MacOS/Autostart/SharedListFileAutoStartRegistrationProvider.cs
@@ -9,8 +9,8 @@ public class SharedListFileAutoStartRegistrationProvider : IAutoStartRegistratio
 {
     private const string GroupId = "de.timo_reymann.unilaunch";
 
-    private string LaunchPListFile => $"{PathUtil.UserHome}/Library/LaunchAgents/{GroupId}";
-    private string AppFile => Path.GetDirectoryName(Process.GetCurrentProcess().MainModule!.FileName)!;
+    private string LaunchPListFile => $"{PathUtil.UserHome}/Library/LaunchAgents/{GroupId}.plist";
+    private string AppFile => Path.GetDirectoryName(Process.GetCurrentProcess().MainModule!.FileName)! + "/UniLaunch.MacOS";
 
     public void Register( List<string> arguments)
     {
@@ -18,6 +18,7 @@ public class SharedListFileAutoStartRegistrationProvider : IAutoStartRegistratio
         writer.WriteString("Label", GroupId);
         writer.WriteArray("ProgramArguments", new[] { AppFile }.Concat(arguments).ToArray());
         writer.WriteBool("RunAtLoad", true);
+        writer.WriteBool("AbandonProcessGroup", true);
         writer.WriteString("StandardOutPath", $"{PathUtil.UserHome}/Library/Logs/UniLaunch/launchd-stdout.log");
         writer.WriteString("StandardErrorPath", $"{PathUtil.UserHome}/Library/Logs/UniLaunch/launchd-stderr.log");
     }
