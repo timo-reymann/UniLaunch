29fc3be8df21e437ff2fdde8d791a9fd638d6ca5
Timo Reymann <timo.reymann@deepl.com>
2024-02-01 20:02:34 +0100

    feat: Add dummy editor ui menu bar

--- UniLaunch.UI/Views/MainWindow.axaml.cs
+++ UniLaunch.UI/Views/MainWindow.axaml.cs
@@ -1,18 +1,50 @@
 using System;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace UniLaunch.UI.Views;
 
 public partial class MainWindow : Window
 {
+    private bool _mouseDownForWindowMoving;
+    private PointerPoint _originalPoint;
+    
     public MainWindow()
     {
         InitializeComponent();
-        
-        // only use native menu on macos
-        if (OperatingSystem.IsMacOS())
+    }
+
+    private void InputElement_OnPointerMoved(object? sender, PointerEventArgs e)
+    {
+        if (!_mouseDownForWindowMoving)
         {
-            InlineAppMenu.IsVisible = false;
+            return;
         }
+
+        var currentPoint = e.GetCurrentPoint(this);
+        Position = new PixelPoint(Position.X + (int)(currentPoint.Position.X - _originalPoint.Position.X),
+            Position.Y + (int)(currentPoint.Position.Y - _originalPoint.Position.Y));
+    }
+
+    private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.Source is not Panel && e.Source is not Image)
+        {
+            return;
+        }
+        
+        if (WindowState is WindowState.Maximized or WindowState.FullScreen)
+        {
+            return;
+        }
+
+        _mouseDownForWindowMoving = true;
+        _originalPoint = e.GetCurrentPoint(this);
+    }
+
+    private void InputElement_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
+    {
+        _mouseDownForWindowMoving = false;
     }
 }
