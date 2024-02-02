4f462a8743d4c80d245946909acb2b6a346bf4a7
Timo Reymann <timo.reymann@deepl.com>
2024-02-02 12:44:58 +0100

    feat: Add templated custom control for tab item with icon

---
+++ UniLaunch.UI/Controls/TabItemWithIcon.axaml.cs
@@ -0,0 +1,38 @@
+using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+
+namespace UniLaunch.UI.Controls;
+
+public class TabItemWithIcon : TemplatedControl
+{
+    public static readonly DirectProperty<TabItemWithIcon, string> TitleProperty =
+        AvaloniaProperty.RegisterDirect<TabItemWithIcon, string>(
+            "Title",
+            o => o.Title,
+            (o, v) => o.Title = v,
+            defaultBindingMode: BindingMode.OneWay);
+    
+    private string _title;
+
+    public string Title
+    {
+        get => _title;
+        set => SetAndRaise(TitleProperty, ref _title, value);
+    }
+    
+    public static readonly DirectProperty<TabItemWithIcon, string> IconProperty =
+        AvaloniaProperty.RegisterDirect<TabItemWithIcon, string>(
+            "Icon",
+            o => o.Icon,
+            (o, v) => o.Icon = v,
+            defaultBindingMode: BindingMode.OneWay);
+    
+    private string _icon;
+
+    public string Icon
+    {
+        get => _icon;
+        set => SetAndRaise(TitleProperty, ref _icon, value);
+    }
+}
