c81e4ff45870b06dc34db1b06f5f9f8c571c3e10
Timo Reymann <timo.reymann@deepl.com>
2024-02-08 15:53:42 +0100

    feat: Add control for app target

--- UniLaunch.MacOS/ViewModel/AppFileTargetViewModel.cs
+++ UniLaunch.MacOS/ViewModel/AppFileTargetViewModel.cs
@@ -1,10 +1,21 @@
-using ReactiveUI;
+using Avalonia.Platform.Storage;
+using UniLaunch.MacOS.Controls;
 using UniLaunch.MacOS.Targets;
 using UniLaunch.UI.CodeGeneration;
 
 namespace UniLaunch.MacOS.ViewModel;
 
-[GenerateViewModel(typeof(AppFileTarget))]
+[GenerateViewModel(typeof(AppFileTarget), typeof(AppFileTargetControl))]
 public partial class AppFileTargetViewModel
 {
+    public FilePickerOpenOptions FilePickerOpenOptions { get; } = new()
+    {
+        FileTypeFilter =
+        [
+            new("App-File")
+            {
+                AppleUniformTypeIdentifiers = ["applicationBundle"]
+            }
+        ]
+    };
 }
