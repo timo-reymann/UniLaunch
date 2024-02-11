a69418787707225806ae6bcc4576a5ef7a0e2ce9
Timo Reymann <timo.reymann@deepl.com>
2024-02-11 18:37:11 +0100

    feat: Add draft rule set UI

--- UniLaunch.UI/ViewModels/RulesetViewModel.cs
+++ UniLaunch.UI/ViewModels/RulesetViewModel.cs
@@ -1,10 +1,11 @@
 using ReactiveUI;
 using UniLaunch.Core.Rules;
 using UniLaunch.UI.CodeGeneration;
+using UniLaunch.UI.Controls;
 
 namespace UniLaunch.UI.ViewModels;
 
-[GenerateViewModel(typeof(RuleSet))]
+[GenerateViewModel(typeof(RuleSet), typeof(RuleSetControl))]
 public partial class RulesetViewModel
 {
     
