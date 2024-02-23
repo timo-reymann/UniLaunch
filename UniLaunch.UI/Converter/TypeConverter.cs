dcd1bbd20b60c0cf1031c15beb9341fb94b9202f
Timo Reymann <timo.reymann@deepl.com>
2024-02-23 15:12:57 +0100

    feat: Add button for ruleset append

--- UniLaunch.UI/Converter/TypeConverter.cs
+++ UniLaunch.UI/Converter/TypeConverter.cs
@@ -1,5 +1,6 @@
 using System;
 using Avalonia.Data.Converters;
+using UniLaunch.Core.Rules;
 using UniLaunch.Core.Targets;
 
 namespace UniLaunch.UI.Converter;
@@ -7,11 +8,12 @@ namespace UniLaunch.UI.Converter;
 public static class TypeConverter
 {
     public static readonly IValueConverter TargetType = new FuncValueConverter<Type, string>(ConvertTargetType);
+    public static readonly IValueConverter RuleType = new FuncValueConverter<Type, string>(ConvertRuleType);
 
     // Workaround converter to fix the freaking broken ListBox scroll and gridsplitter shit
     public static readonly IValueConverter ListBoxHeightConverter =
-        new FuncValueConverter<double, double>( height => height - 30.0);
-    
+        new FuncValueConverter<double, double>(height => height - 30.0);
+
     private static string ConvertTargetType(Type? t)
     {
         if (t == null)
@@ -21,5 +23,16 @@ public static class TypeConverter
 
         var instance = Activator.CreateInstance(t) as Target;
         return instance?.TargetType ?? string.Empty;
+    }
+
+    private static string ConvertRuleType(Type? t)
+    {
+        if (t == null)
+        {
+            return string.Empty;
+        }
+
+        var instance = Activator.CreateInstance(t) as Rule;
+        return instance?.RuleName ?? string.Empty;
     }
 }
--- UniLaunch.UI/ViewModels/RulesetViewModel.cs
+++ UniLaunch.UI/ViewModels/RulesetViewModel.cs
@@ -1,7 +1,10 @@
-using System.Collections.Generic;
+using System;
+using System.Collections.Immutable;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
 using ReactiveUI;
+using UniLaunch.Core.Autostart;
 using UniLaunch.Core.Rules;
 using UniLaunch.UI.CodeGeneration;
 using UniLaunch.UI.Controls;
@@ -11,6 +14,9 @@ namespace UniLaunch.UI.ViewModels;
 [GenerateViewModel(typeof(RuleSet), typeof(RuleSetControl))]
 public partial class RulesetViewModel
 {
+    public ImmutableHashSet<Type> EnabledRuleTypes => UniLaunchEngine.Instance.EnabledRuleTypes;
+    public ICommand AddRule { get; private set; }
+
     public ObservableCollection<BaseEntityViewModel> RulesListProperty
     {
         get => new(EntityViewModelRegistry.Instance.Of(_model.Rules));
@@ -19,5 +25,18 @@ public partial class RulesetViewModel
             _model.Rules = value.Select(m => (Rule)m.Model).ToList();
             this.RaisePropertyChanged();
         }
+    }
+
+    partial void InitViewModel()
+    {
+        AddRule = ReactiveCommand.Create<Type>(_AddRule);
+    }
+
+    private void _AddRule(Type t)
+    {
+        var rule = Activator.CreateInstance(t) as Rule;
+        RulesListProperty.Add(rule!.ToViewModel()!);
+        _model.Rules.Add(rule!);
+        this.RaisePropertyChanged(nameof(RulesListProperty));
     }
 }
