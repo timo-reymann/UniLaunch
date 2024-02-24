e4a0f6a0fcded115e995c7bc395bfc5f2176886d
Timo Reymann <timo.reymann@deepl.com>
2024-02-24 19:24:23 +0100

    chore: Fix version in cask

--- Casks/unilaunch.rb
+++ Casks/unilaunch.rb
@@ -1,6 +1,6 @@
 cask "unilaunch" do 
-  version "0.0.2" 
-  url "https://github.com/timo-reymann/UniLaunch/releases/download/0.0.2/UniLaunchInstaller-Silicon.dmg" 
+  version "0.0.3" 
+  url "https://github.com/timo-reymann/UniLaunch/releases/download/0.0.3/UniLaunchInstaller-Silicon.dmg" 
   sha256 "13c0e12e8c5c1b0f244c087bbcbeeb5811e8d8484ccc2970fd4411019df940c4" 
   name "UniLaunch" 
   homepage "https://github.com/timo-reymann/UniLaunch" 
