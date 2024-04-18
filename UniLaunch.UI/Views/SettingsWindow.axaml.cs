094fd13566b6057365b521c01ec7b22ff470df2b
Timo Reymann <timo.reymann@deepl.com>
2024-04-18 09:45:28 +0200

    feat: Extend settings dialog with connectivity config

--- UniLaunch.UI/Views/SettingsWindow.axaml.cs
+++ UniLaunch.UI/Views/SettingsWindow.axaml.cs
@@ -3,6 +3,7 @@ using MsBox.Avalonia;
 using MsBox.Avalonia.Enums;
 using UniLaunch.Core.Storage;
 using UniLaunch.UI.Services;
+using UniLaunch.UI.ViewModels;
 
 namespace UniLaunch.UI.Views;
 
@@ -13,8 +14,13 @@ public partial class SettingsWindow : Window
         InitializeComponent();
     }
 
+    public bool HasConfigChangesForLoadedConfigFile { get; private set; }
+
     private async void OnClose(object? sender, WindowClosingEventArgs e)
     {
+        HasConfigChangesForLoadedConfigFile =
+            (DataContext as SettingsViewModel)!.HasChangesForConnectivityCheckConfig;
+        
         if (e.IsProgrammatic)
         {
             return;
