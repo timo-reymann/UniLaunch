7aeed36cf1771575c804882e832ede6d1cfbc95b
Timo Reymann <timo.reymann@deepl.com>
2024-04-19 14:03:03 +0200

    chore: Make PlistDictWriter writer instance private

--- UniLaunch.MacOS/Plist/PlistDictWriter.cs
+++ UniLaunch.MacOS/Plist/PlistDictWriter.cs
@@ -4,27 +4,27 @@ namespace UniLaunch.MacOS.Plist;
 
 public class PlistDictWriter : IDisposable
 {
-    private XmlWriter writer;
+    private readonly XmlWriter _writer;
 
     public PlistDictWriter(string filePath)
     {
-        writer = XmlWriter.Create(new FileStream(filePath, FileMode.Create), new XmlWriterSettings { Indent = true });
-        writer.WriteDocType(
+        _writer = XmlWriter.Create(new FileStream(filePath, FileMode.Create), new XmlWriterSettings { Indent = true });
+        _writer.WriteDocType(
             "plist",
             "-//Apple//DTD PLIST 1.0//EN",
             "http://www.apple.com/DTDs/PropertyList-1.0.dtd",
             null
         );
-        writer.WriteStartElement("plist");
-        writer.WriteAttributeString("version", "1.0");
-        writer.WriteStartElement("dict");
+        _writer.WriteStartElement("plist");
+        _writer.WriteAttributeString("version", "1.0");
+        _writer.WriteStartElement("dict");
     }
 
     private void WriteTag(string key, string content)
     {
-        writer.WriteStartElement(key);
-        writer.WriteString(content);
-        writer.WriteEndElement();
+        _writer.WriteStartElement(key);
+        _writer.WriteString(content);
+        _writer.WriteEndElement();
     }
 
     private void WriteKey(string name)
@@ -41,27 +41,27 @@ public class PlistDictWriter : IDisposable
     public void WriteBool(string key, bool value)
     {
         WriteKey(key);
-        writer.WriteStartElement(value ? "true" : "false");
-        writer.WriteEndElement();
+        _writer.WriteStartElement(value ? "true" : "false");
+        _writer.WriteEndElement();
     }
 
     public void WriteArray(string key, string[] values)
     {
         WriteKey(key);
-        writer.WriteStartElement("array");
+        _writer.WriteStartElement("array");
 
         foreach (var value in values)
         {
             WriteTag("string", value);
         }
 
-        writer.WriteEndElement();
+        _writer.WriteEndElement();
     }
 
     public void Dispose()
     {
-        writer.WriteEndElement();
-        writer.WriteEndElement();
-        writer.Close();
+        _writer.WriteEndElement();
+        _writer.WriteEndElement();
+        _writer.Close();
     }
 }
