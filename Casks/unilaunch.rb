9ef231ae3658a2a39664cdfa127255587d943372
CircleCI <no-reply@circleci.com>
2026-05-02 16:06:36 +0000

    chore: Update cask for 1.7.1
    [skip ci]

--- Casks/unilaunch.rb
+++ Casks/unilaunch.rb
@@ -1,7 +1,7 @@
 cask "unilaunch" do 
-  version "1.7.0" 
-  url "https://github.com/timo-reymann/UniLaunch/releases/download/1.7.0/UniLaunchInstaller-Silicon.dmg" 
-  sha256 "224fa17cd6f917a41d020cbdeeca51b6865aa9806f25859dc8678a7f303fa2dd" 
+  version "1.7.1" 
+  url "https://github.com/timo-reymann/UniLaunch/releases/download/1.7.1/UniLaunchInstaller-Silicon.dmg" 
+  sha256 "cf9e59cc8d976ae909559395760c56475e071765f7236485c864a1f41476dce3" 
   name "UniLaunch" 
   homepage "https://github.com/timo-reymann/UniLaunch" 
   app "UniLaunch.app" 
